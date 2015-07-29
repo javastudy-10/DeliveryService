b58fdce3aaf8cf35167e5f5ad9d8709a2fc10204
Tanya <Tanya@Starr>
2015-07-29 13:32:52 +0300

    Revert "Revert "some small changes in controller Doroshenko""
    This reverts commit 2ca97cc2793e4b5d21a46f17dccfa5fbc52546b2.

--- src/main/java/com/javastudy/deliveryservice/mvc/controller/Doroshenko.java
+++ src/main/java/com/javastudy/deliveryservice/mvc/controller/Doroshenko.java
@@ -30,6 +30,7 @@ public class Doroshenko {
         names.add("misha");
         names.add("gosha");
         names.add("vika");
+        names.add("zhenya");
         
         model.addAttribute("list", names);
         return "doroshenko";
