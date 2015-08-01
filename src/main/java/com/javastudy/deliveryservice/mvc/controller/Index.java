a1930d0c4d6fc026b2bf559fb8d3982d1b6c6889
Tanya <Tanya@Starr>
2015-08-01 13:43:44 +0300

    return files

---
+++ src/main/java/com/javastudy/deliveryservice/mvc/controller/Index.java
@@ -0,0 +1,15 @@
+package com.javastudy.deliveryservice.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class Index {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome() {
+        return "index";
+    }
+}
